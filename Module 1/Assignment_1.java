97a0bee298aa990945485b07b9a83229842d4354
Zkish <zachary.kish@gmail.com>
2024-01-13 06:45:57 -0500

    adding in JoptionPane so that popup will work

--- Module 1/Assignment_1.java
+++ Module 1/Assignment_1.java
@@ -1,6 +1,13 @@
+// import needed for popup
+import javax.swing.JoptionPane;
+
 public class Welcome{
     // diplay the text
     public static void main(String[] args {
         system.out.println("This is my first program that i wrote in java.");
     }
+}
+
+public class Popup{
+    
 }
